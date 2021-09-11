0ab47f3646ea12774ceba1bf65c0443ee328bf9c
Simon Willison <swillison@gmail.com>
2021-09-10 19:27:42 -0700

    Don't include _internal, closes #2

--- datasette_schema_versions/__init__.py
+++ datasette_schema_versions/__init__.py
@@ -5,9 +5,10 @@ from datasette.utils.asgi import Response
 async def schema_versions(datasette):
     schema_versions = {}
     for name, database in datasette.databases.items():
-        schema_versions[name] = (
-            await database.execute("PRAGMA schema_version")
-        ).first()[0]
+        if name != "_internal":
+            schema_versions[name] = (
+                await database.execute("PRAGMA schema_version")
+            ).first()[0]
     return Response.json(schema_versions)
 
 
