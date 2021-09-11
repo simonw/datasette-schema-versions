dded7bba22c6013cd1f73e9b7e3478894f1b9e48
Simon Willison <swillison@gmail.com>
2021-09-10 19:41:23 -0700

    /-/schema-versions.json and /-/schema-versions plus menu item
    Closes #1, closes #3

--- datasette_schema_versions/__init__.py
+++ datasette_schema_versions/__init__.py
@@ -1,17 +1,49 @@
 from datasette import hookimpl
 from datasette.utils.asgi import Response
+import json
 
 
-async def schema_versions(datasette):
+async def schema_versions(datasette, request):
+    return Response.html(
+        await datasette.render_template(
+            "show_json.html",
+            {
+                "filename": "schema-versions.json",
+                "data_json": json.dumps(await _schema_versions(datasette), indent=4),
+            },
+            request=request,
+        )
+    )
+
+
+async def schema_versions_json(datasette):
+    return Response.json(await _schema_versions(datasette))
+
+
+async def _schema_versions(datasette):
     schema_versions = {}
     for name, database in datasette.databases.items():
         if name != "_internal":
             schema_versions[name] = (
                 await database.execute("PRAGMA schema_version")
             ).first()[0]
-    return Response.json(schema_versions)
+    return schema_versions
+
+
+@hookimpl
+def menu_links(datasette, actor):
+    if actor and actor.get("id") == "root":
+        return [
+            {
+                "href": datasette.urls.path("/-/schema-versions"),
+                "label": "Schema versions",
+            },
+        ]
 
 
 @hookimpl
 def register_routes():
-    return [("^/-/schema-versions$", schema_versions)]
+    return [
+        (r"^/-/schema-versions$", schema_versions),
+        (r"^/-/schema-versions\.json$", schema_versions_json),
+    ]
--- tests/test_schema_versions.py
+++ tests/test_schema_versions.py
@@ -4,35 +4,38 @@ import pytest
 import sqlite_utils
 
 
-@pytest.mark.asyncio
-async def test_plugin_is_installed():
-    app = Datasette([], memory=True).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/plugins.json")
-        assert 200 == response.status_code
-        installed_plugins = {p["name"] for p in response.json()}
-        assert "datasette-schema-versions" in installed_plugins
-
-
-@pytest.mark.asyncio
-async def test_schema_versions(tmpdir):
+@pytest.fixture
+def datasette_and_dbs(tmpdir):
     path1 = str(tmpdir / "data1.db")
     path2 = str(tmpdir / "data2.db")
     db1 = sqlite_utils.Database(path1)
     db1["foo"].insert({"bar": 1})
     db2 = sqlite_utils.Database(path2)
     db2["foo"].insert({"bar": 1})
-    app = Datasette([path1, path2]).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/schema-versions")
-        assert 200 == response.status_code
-        assert response.json() == {"data1": 1, "data2": 1}
-        # Inserting records should change nothing
-        db1["foo"].insert({"bar": 1})
-        db2["foo"].insert({"bar": 1})
-        response = await client.get("http://localhost/-/schema-versions")
-        assert response.json() == {"data1": 1, "data2": 1}
-        # But modifying the schema should cause a change
-        db1["baz"].insert({"baz": 1})
-        response = await client.get("http://localhost/-/schema-versions")
-        assert response.json() == {"data1": 2, "data2": 1}
+    return Datasette([path1, path2]), db1, db2
+
+
+@pytest.mark.asyncio
+async def test_schema_versions_json(datasette_and_dbs):
+    datasette, db1, db2 = datasette_and_dbs
+    response = await datasette.client.get("/-/schema-versions.json")
+    assert 200 == response.status_code
+    assert response.json() == {"data1": 1, "data2": 1}
+    # Inserting records should change nothing
+    db1["foo"].insert({"bar": 1})
+    db2["foo"].insert({"bar": 1})
+    response = await datasette.client.get("/-/schema-versions.json")
+    assert response.json() == {"data1": 1, "data2": 1}
+    # But modifying the schema should cause a change
+    db1["baz"].insert({"baz": 1})
+    response = await datasette.client.get("/-/schema-versions.json")
+    assert response.json() == {"data1": 2, "data2": 1}
+
+
+@pytest.mark.asyncio
+async def test_schema_versions(datasette_and_dbs):
+    datasette = datasette_and_dbs[0]
+    response = await datasette.client.get("/-/schema-versions")
+    assert response.status_code == 200
+    assert "<h1>schema-versions.json</h1>" in response.text
+    assert "<pre>{" in response.text
